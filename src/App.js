a1613d254956ae51ab1de805e08d853e35675dff
Spencer Kyonka <spencer.kyonka@rangle.io>
2024-03-17 17:16:14 -0400

    Clear up import errors, language client still errorful

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Something from './components/Something';
+// import Another from './components/Another';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Something.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Something = ({ someNumb }) => {
+
+  return (
+    <div>
+      Here is Something
+      <h2>{someNumb}</h2>
+    </div>
+  )
+}
+
+
+export default Something;
